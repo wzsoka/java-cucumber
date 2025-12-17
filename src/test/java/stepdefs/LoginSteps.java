a96758d85261ce5f6c93e2589bec54b36888ff4b
wzsoka <szwzsoka@gmail.com>
2025-12-18 00:17:48 +0100

    Update LoginSteps.java

--- src/test/java/stepdefs/LoginSteps.java
+++ src/test/java/stepdefs/LoginSteps.java
@@ -8,7 +8,8 @@ import io.cucumber.java.hu.És;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import pages.LoginPage;
+import pages.StartPage;
+import pages.components.LoginModal;
 
 import static stepdefs.Hooks.driver;
 
@@ -16,33 +17,36 @@ public class LoginSteps {
 
     @Amikor("Bejelentkezek korábban létrehozott felhasználóval")
     public void bejelentkezekKorábbanLétrehozottFelhasználóval() {
-        LoginPage loginPage = new LoginPage(driver);
+        LoginModal loginModal = new LoginModal(driver);
         ConfigFileReader configFileReader = new ConfigFileReader();
         String savedUser = configFileReader.getConfigValue("savedUser");
         String pw = configFileReader.getConfigValue("password");
-        loginPage.login(savedUser, pw);
+        loginModal.login(savedUser, pw);
     }
 
     @Amikor("Bejelentkezek {string} és {string} adatokkal")
     public void bejelentkezekÉsAdatokkal(String username, String password) {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(username, password);
+        LoginModal loginModal = new LoginModal(driver);
+        loginModal.login(username, password);
     }
 
     @Akkor("{string} szöveg jelenik meg")
     public void szövegJelenikMeg(String errorMessage) {
         WebElement message;
-        if (errorMessage.equals("Kérjük, ellenőrizd a megadott adatokat.")){
-            message = driver.findElement(By.cssSelector("div.global-alert-message.error"));
+        if (errorMessage.equals("A bejelentkezés sikertelen. Kérjük, ellenőrizd bejelentkezési adataidat, és próbáld meg újra.")){
+            message = driver.findElement(By.cssSelector("div.message"));
         } else {
-            message = driver.findElement(By.cssSelector("span.form-control-errors"));
+            message = driver.findElement(By.xpath("//label[text()='Jelszó']/following-sibling::span[@class='errordisplay']"));
         }
         Assertions.assertEquals(errorMessage, message.getText().trim());
     }
 
-    @És("A bejelentkezés oldalon maradok")
-    public void aBejelentkezésOldalonMaradok() {
-        LoginPage loginPage = new LoginPage(driver);
-        Assertions.assertTrue(loginPage.loginButton.isDisplayed());
+    @És("A bejelentkezés oldalon maradok, amit bezárok")
+    public void aBejelentkezésOldalonMaradokAmitBezárok() {
+        LoginModal loginModal = new LoginModal(driver);
+        Assertions.assertTrue(loginModal.loginButton.isDisplayed());
+        loginModal.closeLoginModal();
+        StartPage startPage = new StartPage(driver);
+        Assertions.assertTrue(startPage.headerTopbar.accountButton.isEnabled());
     }
 }
