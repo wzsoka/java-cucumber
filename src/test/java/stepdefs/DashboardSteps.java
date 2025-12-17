92b574f14891f1c26faec7bba2c78b90e3471711
wzsoka <szwzsoka@gmail.com>
2025-12-18 00:17:08 +0100

    Update DashboardSteps.java

--- src/test/java/stepdefs/DashboardSteps.java
+++ src/test/java/stepdefs/DashboardSteps.java
@@ -3,10 +3,15 @@ package stepdefs;
 import io.cucumber.java.hu.Akkor;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.DashboardPage;
 import pages.StartPage;
+
+import java.time.Duration;
 
 import static stepdefs.Hooks.driver;
 
@@ -16,35 +21,34 @@ public class DashboardSteps {
     public void aFiókombaJutok() {
         assertLoggedIn();
         DashboardPage dashboardPage = new DashboardPage(driver);
-        new Actions(driver).moveToElement(dashboardPage.headerTopbar.accountIcon).build().perform();
-        Assertions.assertTrue(dashboardPage.accountMenuTitle.isDisplayed());
-        dashboardPage.headerTopbar.logout();
-        assertLoggedOut();
+        Assertions.assertTrue(dashboardPage.headerTopbar.accountButton.isDisplayed());
+        Assertions.assertTrue(dashboardPage.ordersButton.isDisplayed());
+        new Actions(driver).moveToElement(dashboardPage.logoutButton).build().perform();
+        logout();
     }
 
     @Akkor("A fiókom létrejön")
     public void aFiókomLétrejön() {
-        assertRegistered();
+        assertLoggedIn();
         DashboardPage dashboardPage = new DashboardPage(driver);
-        new Actions(driver).moveToElement(dashboardPage.headerTopbar.accountIcon).build().perform();
-        Assertions.assertTrue(dashboardPage.newAccountHeader.isDisplayed());
-        dashboardPage.headerTopbar.logout();
-        assertLoggedOut();
-    }
-
-    private void assertRegistered(){
-        WebElement message = driver.findElement(By.cssSelector("div.global-alert-message.success"));
-        Assertions.assertEquals("Sikeres regisztráció.", message.getText().trim());
+        Assertions.assertTrue(dashboardPage.headerTopbar.accountButton.isDisplayed());
+        Assertions.assertTrue(dashboardPage.ordersButton.isDisplayed());
+        new Actions(driver).moveToElement(dashboardPage.customerNumber).build().perform();
+        logout();
     }
 
     private void assertLoggedIn(){
-        WebElement message = driver.findElement(By.cssSelector("div.global-alert-message.success"));
-        Assertions.assertEquals("Sikeres bejelentkezés.", message.getText().trim());
+        WebElement message = driver.findElement(By.cssSelector("div.toast-inner"));
+        Assertions.assertEquals("Sikeres bejelentkezés", message.getText().trim());
     }
 
-    private void assertLoggedOut(){
+    private void logout(){
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        WebElement element = driver.findElement(By.cssSelector("button.logout"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.elementToBeClickable(dashboardPage.logoutButton)).click();
         StartPage startPage = new StartPage(driver);
-        new Actions(driver).moveToElement(startPage.headerTopbar.accountIcon).build().perform();
-        Assertions.assertTrue(startPage.headerTopbar.menuItemLogin.isEnabled());
+        new Actions(driver).moveToElement(startPage.headerTopbar.accountButton).build().perform();
+        Assertions.assertTrue(startPage.headerTopbar.accountButton.isEnabled());
     }
 }
