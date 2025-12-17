3d2021885155327566e430892ed5e0f4c00c2b70
wzsoka <szwzsoka@gmail.com>
2025-12-18 00:15:59 +0100

    Update StartPage.java

--- src/main/java/pages/StartPage.java
+++ src/main/java/pages/StartPage.java
@@ -1,10 +1,15 @@
 package pages;
 
+import dataProvider.ConfigFileReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.components.HeaderTopbar;
+
+import java.time.Duration;
 
 public class StartPage {
 
@@ -23,6 +28,9 @@ public class StartPage {
 
     public StartPage(WebDriver driver) {
         this.driver = driver;
+        ConfigFileReader configFileReader = new ConfigFileReader();
+        String HOME_URL = configFileReader.getConfigValue("homeUrl");
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.urlToBe(HOME_URL));
         this.headerTopbar = new HeaderTopbar(this.driver);
         PageFactory.initElements(this.driver, this);
     }
