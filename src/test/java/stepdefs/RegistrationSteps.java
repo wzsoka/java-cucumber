d313eb24d61b7077b19952374e4b686fa3707ef5
wzsoka <szwzsoka@gmail.com>
2025-12-18 00:19:56 +0100

    Update RegistrationSteps.java

--- src/test/java/stepdefs/RegistrationSteps.java
+++ src/test/java/stepdefs/RegistrationSteps.java
@@ -15,7 +15,7 @@ public class RegistrationSteps {
     @Amikor("Teljesen kitöltöm és beküldöm a regisztrációt")
     public void teljesenKitöltömÉsBeküldömARegisztrációt() {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registerAsPerson();
+        registrationPage.registerNewUser();
     }
 
     @Amikor("Hiányosan kitöltöm és beküldöm a regisztrációt")
@@ -27,14 +27,14 @@ public class RegistrationSteps {
     @Amikor("Korábban regisztrált email címmel próbálok regisztrálni")
     public void korábbanRegisztráltEmailCímmelPróbálokRegisztrálni() {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registerWithSavedUser();
+        registrationPage.registerWithOccupiedEmail();
     }
 
     @Akkor("Hibaüzenet jelzi a kitöltendő mezőt")
     public void hibaüzenetJelziAKitöltendőMezőt() {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        String errorText = "A mező kitöltése kötelező.";
-        Assertions.assertEquals(errorText, registrationPage.errorMessages.get(0).getText().trim());
+        String errorText = "Kötelező mező";
+        Assertions.assertEquals(errorText, registrationPage.errorMessage.getText().trim());
     }
 
     @És("A Regisztráció oldalon maradok")
@@ -45,8 +45,8 @@ public class RegistrationSteps {
 
     @Akkor("Hibaüzenet jelzi hogy már létezik fiók az email címmel")
     public void hibaüzenetJelziHogyMárLétezikFiókAzEmailCímmel() {
-        WebElement message = driver.findElement(By.cssSelector("span.form-control-errors"));
-        String errorText = "Ezzel az e-mail címmel már létezik felhasználói fiók.";
+        WebElement message = driver.findElement(By.xpath("//div[@data-testid='global-alert-message-error']"));
+        String errorText = "Sikertelen regisztráció";
         Assertions.assertEquals(errorText, message.getText().trim());
     }
 }
